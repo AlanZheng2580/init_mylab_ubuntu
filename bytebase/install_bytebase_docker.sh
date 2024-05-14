59fec94dcc0c0f8f70612d375f437b57cc04709e
AlanZheng2580 <cyching@tsmc.com>
2024-05-14 09:46:27 +0800

    init bytebase

---
+++ bytebase/install_bytebase_docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#reference: https://www.bytebase.com/docs/get-started/self-host/#docker
+set -xe
+
+docker run -d --rm --init \
+	--name bytebase \
+	--publish 8080:8080 --pull always \
+	--volume ~/.bytebase/data:/var/opt/bytebase \
+	bytebase/bytebase:2.16.0
+#       cyching/bytebase:2.16.0-fmt-0510-2
---
+++ bytebase/install_bytebase_source.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#reference: https://www.bytebase.com/docs/get-started/self-host/#docker
+set -xe
+
+mkdir -p ~/bytebase
+
+pushd ~/bytebase
+
+sudo apt update
+sudo apt install -y libkrb5-dev
+
+export DOCKER_BUILDKIT=0
+echo "export DOCKER_BUILDKIT=0" >> ~/.bashrc
+
+# cd ~/bytebase
+git clone https://github.com/cyching/bytebase.git
+cd bytebase/scripts
+git checkout fork/2.16.0
+sed -i '/Dockerfile/a --build-arg TARGETARCH="amd64" \\' ~/bytebase/bytebase/scripts/build_bytebase_docker.sh
+./build_bytebase_docker.sh
+
+popd
+
+# add "PATH=/usr/bin:/usr/local/go/bin:$PATH"
