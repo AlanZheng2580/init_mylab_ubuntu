ee8baa5c72994cd1d77e9bebf66388b68ed0b792
AlanZheng2580 <cyching@tsmc.com>
2024-05-15 08:43:28 +0800

    update mysql.sh

--- bytebase/mysql.sh
+++ bytebase/mysql.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -v ~/mysqldatatest:/var/lib/mysql -v ~/my.cnf:/etc/my.cnf --name mysql-test -p 3307:3306 -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql:5.7
-docker run -v ~/mysqldataprod:/var/lib/mysql -v ~/my.cnf:/etc/my.cnf --name mysql-prod -p 3308:3306 -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql:5.7
+docker run --rm -v ~/mysqldatatest:/var/lib/mysql -v ~/my.cnf:/etc/my.cnf --name mysql-test -p 3307:3306 -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql:5.7
+docker run --rm -v ~/mysqldataprod:/var/lib/mysql -v ~/my.cnf:/etc/my.cnf --name mysql-prod -p 3308:3306 -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql:5.7
