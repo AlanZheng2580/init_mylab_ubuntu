03ab1b48672e97dd8138a629a7bf61866c7b2642
AlanZheng2580 <168727160+AlanZheng2580@users.noreply.github.com>
2024-05-28 08:29:55 +0800

    WIP

--- bytebase/install_bytebase_docker.sh
+++ bytebase/install_bytebase_docker.sh
@@ -6,6 +6,6 @@ set -xe
 docker run -d --rm --init \
 	--name bytebase \
 	--publish 8080:8080 --pull always \
-	--volume ~/.bytebase/data:/var/opt/bytebase \
+	--volume ~/.bytebase:/var/opt/bytebase \
 	bytebase/bytebase:2.16.0
 #       cyching/bytebase:2.16.0-fmt-0510-2
--- bytebase/mysql.sh
+++ bytebase/mysql.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker run --rm -v ~/mysqldatatest:/var/lib/mysql -v ~/my.cnf:/etc/my.cnf --name mysql-test -p 3307:3306 -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql:5.7
-docker run --rm -v ~/mysqldataprod:/var/lib/mysql -v ~/my.cnf:/etc/my.cnf --name mysql-prod -p 3308:3306 -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql:5.7
+WORKSPACE_TSMC=~/workspace/tsmc
+docker run --rm -v ${WORKSPACE_TSMC}/mysqldatatest:/var/lib/mysql -v ${WORKSPACE_TSMC}/my.cnf:/etc/my.cnf --name mysql-test -p 3307:3306 -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql:5.7
+docker run --rm -v ${WORKSPACE_TSMC}/mysqldataprod:/var/lib/mysql -v ${WORKSPACE_TSMC}/my.cnf:/etc/my.cnf --name mysql-prod -p 3308:3306 -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql:5.7
